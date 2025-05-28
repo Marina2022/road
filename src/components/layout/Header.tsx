5ecd605cf44ec734672bb5a1e0def73dba6d12e2
Marina2022 <marusiiiia@yandex.ru>
2025-05-28 09:19:47 +0300

    header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from "next/link";
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, buttonVariants} from "@/components/ui/button";
 import ToggleTheme from './ToggleTheme';
 import UserMenu from "@/components/layout/UserMenu";
@@ -9,13 +9,24 @@ import {useAuth} from "@/hooks/use-auth";
 
 const Header = () => {
 
-
   const {user, fetched} = useAuth()
+  const [shouldAnimate, setShouldAnimate] = useState(false)
+
+  useEffect(() => {
+    // включаем анимацию только после монтирования
+    const timeout = setTimeout(() => {
+      setShouldAnimate(true)
+    }, 300) // маленькая задержка — после гидратации
+
+    return () => clearTimeout(timeout)
+  }, [])
+
+  
 
   if (!fetched) return null
 
   return (
-    <nav className="flex justify-between p-2 max-w-4/5 mx-auto animate-header-from-top">
+    <nav className={`flex justify-between p-2 max-w-4/5 mx-auto ${shouldAnimate ? 'animate-header-from-top' : '-translate-y-full'}`}>
       <Button asChild variant="outline">
         <Link href="/">Home</Link>
       </Button>
